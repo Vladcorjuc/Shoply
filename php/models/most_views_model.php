4a5d80fbe66453f2301034d5f8f9f390b4dab21c
Vladcorjuc <47830022+Vladcorjuc@users.noreply.github.com>
2020-06-08 14:41:38 +0300

    sunt prost :)))

--- php/models/most_views_model.php
+++ php/models/most_views_model.php
@@ -10,7 +10,7 @@ require_once __DIR__ . "/../../database/database.php";
 function getMostViewedProducts()
 {
     $query = "SELECT image, title, COALESCE(FLOOR(AVG(rating)), 0) AS rating, price, offers,p.link as link FROM products p" .
-        " LEFT JOIN rating r ON p.link = r.product GROUP BY image, title, price, offers,views,p.link ORDER BY views LIMIT 20";
+        " LEFT JOIN rating r ON p.link = r.product GROUP BY image, title, price, offers,views,p.link ORDER BY views DESC LIMIT 20";
     $statement = Database::getConnection()->prepare($query);
     $statement->execute();
     if ($statement->rowCount() == 0) {
